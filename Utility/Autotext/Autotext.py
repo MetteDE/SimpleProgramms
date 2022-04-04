f1d4b6b1cf36f3ae9742df51978725f6bd2a3924
Mathis <mattyyytrd@gmail.com>
2022-04-04 23:35:43 +0200

    Added Autotext and install instructions

---
+++ Utility/Autotext/Autotext.py
@@ -0,0 +1,28 @@
+#imports 
+import time
+import keyboard as k
+from pynput.keyboard import Key
+from pynput.keyboard import Controller
+
+#UCV (User controlled variables)
+keyboard = Controller()
+text = str(input("Text -> "))
+number_of_runs = int(input("Count -> "))
+pause = float(input("Delay (seconds) -> "))
+exit = str(input("Key to exit -> "))
+time.sleep(5)
+runs = 0
+
+
+while runs < number_of_runs:
+    if k.is_pressed(exit):
+        break
+    time.sleep(pause)
+    for char in text:
+        keyboard.press(char)
+        keyboard.release(char)
+    runs += 1 
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    
+print("Done")
